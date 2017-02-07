8a882de683cec6bde0efe060389991ab6c6c2ad9
Willy Verrier <wverrier@isimtl.local>
2017-02-07 10:13:24 -0500

    code sidebar ajout

--- sidebar.php
+++ sidebar.php
@@ -15,3 +15,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
+
+<?php
+//if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+//return;
+//}
+//?>
+<!---->
+<!--<aside id="secondary" class="widget-area" role="complementary">-->
+<!--    --><?php //dynamic_sidebar( 'sidebar-2' ); ?>
+<!--</aside><!-- #secondary -->
+
