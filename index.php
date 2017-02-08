eeebd1d3c097198bc13ba4e953566ad9426445d0
Willy Verrier <wverrier@isimtl.local>
2017-02-08 11:03:12 -0500

    add sur git

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            
+
             <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('top') ) : endif; ?>
 
 		<?php
